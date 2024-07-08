5517fbdd0560d5be0aaf140cfcbacb999daa5fce
Апи <laz.danila17@mail.ru>
2024-07-08 12:46:53 +0300

    больше проверок - больше щиткода!

--- functions.py
+++ functions.py
@@ -15,23 +15,39 @@ async def server_status(address):
         # Формирование Embed ответа для последующей передачи боту.
         print(f"[INFO] {data}")
         embed = disnake.Embed(title="Статус игрового сервера Space Station 14", color=disnake.Color.green())
-        embed.add_field(name="Игровой сервер", value=data['name'], inline=False)
+        embed.set_author(name=data['name'], icon_url="https://raw.githubusercontent.com/space-syndicate/space-station-14/master/Resources/Textures/Logo/icon/icon-128x128.png")
         embed.add_field(name="Игроки", value=f"Игроков {data['players']} из {data['soft_max_players']}", inline=False)
-        
+        # Проверка по статусу игры. Внутри неё ещё дополнительные проверки, тк некоторые сервера не присылают некоторые данные
         if data['run_level'] == 1:
             embed.add_field(name="Статус игры", value="Игра продолжается", inline=True)
-            if 'preset' in data: embed.add_field(name="Режим", value=data['preset'], inline=True)
-            if 'map' in data: embed.add_field(name="Карта", value=data['map'], inline=True)
-            if'round_id' in data: embed.add_field(name="Текущий раунд", value=f"Раунд #{data['round_id']}", inline=True)
-            embed.add_field(name="Время раунда", value=f"{(disnake.utils.utcnow() - disnake.utils.parse_time(data['round_start_time'])).total_seconds() // 60:.0f} минут назад", inline=True)
+            if 'preset' in data: 
+                embed.add_field(name="Режим", value=data['preset'], inline=True)
+            if 'map' in data: 
+                embed.add_field(name="Карта", value=data['map'], inline=True)
+            if 'round_id' in data: 
+                embed.add_field(name="Текущий раунд", value=f"Раунд #{data['round_id']}", inline=True)
+            if 'round_start_time' in data:
+                converted = f"{(disnake.utils.utcnow() - disnake.utils.parse_time(data['round_start_time'])).total_seconds() // 60:.0f}"
+                embed.add_field(name="Время раунда", value=f"Идёт {converted} минут", inline=True)
         elif data['run_level'] == 2:
-            embed.add_field(name="Статус игры", value="Игра заканчивается (Манифест)", inline=False)
+            embed.add_field(name="Статус игры", value="Игра заканчивается - Манифест", inline=False)
+            if 'round_start_time' in data:
+                converted = f"{(disnake.utils.utcnow() - disnake.utils.parse_time(data['round_start_time'])).total_seconds() // 60:.0f}"
+                embed.add_field(name="Время раунда", value=f"Он шёл {converted} минут", inline=True)
         else:
-            embed.add_field(name="Статус игры", value="Игра не началась (Лобби)", inline=False)
-            
-        if data['tags']: embed.add_field(name="Теги сервера", value=", ".join(data['tags']), inline=False)
-        if 'panic_bunker' in data: embed.add_field(name="Режим бункера", value="Действует" if data['panic_bunker'] else "Отключён", inline=True)
-        if 'baby_jail' in data: embed.add_field(name="Режим тюрьмы", value="Действует" if data['baby_jail'] else "Отключён", inline=True) 
+            embed.add_field(name="Статус игры", value="Игра не началась - Лобби", inline=False)
+            if 'preset' in data: 
+                embed.add_field(name="Предустановленный режим", value=data['preset'], inline=True)
+
+        if data['tags']: 
+            embed.add_field(name="Теги сервера", value=", ".join(data['tags']), inline=False)
+        else:
+            embed.add_field(name="Теги сервера", value="Не указаны", inline=False)
+
+        if 'panic_bunker' in data: 
+            embed.add_field(name="Режим бункера", value="Действует" if data['panic_bunker'] else "Отключён", inline=True)
+        if 'baby_jail' in data: 
+            embed.add_field(name="Режим тюрьмы", value="Действует" if data['baby_jail'] else "Отключён", inline=True) 
         return embed
 
     except Exception as e:
--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import json
 # < Файлы проекта >
 from servers import lst
 from functions import server_status
+from secret import secret_token
 
 bot = commands.Bot(command_prefix='!', intents=disnake.Intents.all())
+
 
 ###
 # Команда для получения статуса, из словаря
@@ -21,7 +23,7 @@ async def status_command_dict(ctx, address: str = commands.Param(
         embed = disnake.Embed(title="ЩКЕБЕДЕ ДОП ДОП ЕС ЕС", color=disnake.Color.yellow())
         embed.set_image(url="https://media.tenor.com/WhXYjpREz1kAAAAM/skibidi-toilet-skibidi.gif")
         embed.set_thumbnail(url="https://steamuserimages-a.akamaihd.net/ugc/2038494310206033069/2FFBA39BB754CF0D0FC83C62636BAE30BE992758/")
-        embed.description = "Brrr, skibidi, dom-dom-dom, yes-yes \nSkibidi-dabudu, nib-nib \nSkibidi, dom-dom-dom, yes-yes \nSkibidi-dabudu, nib-nib \nWe ain't here to hurt nobody (skibidi, skibidi, skibidi) \nWanna see you work your body (skibidi, skibidi, skibidi)"
+        embed.description = "Бррр, щкебеде, доп доп доп, ес ес \nСкибиди дабиди да дап \nSkibidi, dom-dom-dom, yes-yes \nSkibidi-dabudu, nib-nib \nWe ain't here to hurt nobody (skibidi, skibidi, skibidi) \nWanna see you work your body (skibidi, skibidi, skibidi)"
         await ctx.send(embed=embed)
     else:
         embed = await server_status(address)
@@ -36,8 +38,6 @@ async def status_command_manually(ctx, address):
     embed = await server_status(address + '/status')
     await ctx.send(embed=embed)
 
-  
-
 
 ###
 # Запуск злоебучего бота
@@ -48,5 +48,4 @@ async def on_ready():
     activity = disnake.Game(name='TREALSIDE')
     await bot.change_presence(status=disnake.Status.online, activity=activity)
 
-
-bot.run('')
+bot.run(secret_token)
