627f9977fa68d7d1ce7efec0a79ad73ecff279a7
Апи <laz.danila17@mail.ru>
2024-07-09 23:04:27 +0300

    Попытки снизить уровень щиткода на квадратный метр

--- functions.py
+++ functions.py
@@ -6,17 +6,18 @@ import disnake
 ###
 async def server_status(address):
     try:
-        print(f"[INFO] {address}")
         # HTTP-запрос на сервер
+        print(f"[LOG] {address}")
         async with aiohttp.ClientSession() as session:
             async with session.get(address) as response:
                 data = await response.json()
 
         # Формирование Embed ответа для последующей передачи боту.
-        print(f"[INFO] {data}")
+        print(f"[LOG] {data}")
         embed = disnake.Embed(title="Статус игрового сервера Space Station 14", color=disnake.Color.green())
         embed.set_author(name=data['name'], icon_url="https://raw.githubusercontent.com/space-syndicate/space-station-14/master/Resources/Textures/Logo/icon/icon-128x128.png")
         embed.add_field(name="Игроки", value=f"Игроков {data['players']} из {data['soft_max_players']}", inline=False)
+        
         # Проверка по статусу игры. Внутри неё ещё дополнительные проверки, тк некоторые сервера не присылают некоторые данные
         if data['run_level'] == 1:
             embed.add_field(name="Статус игры", value="Игра продолжается", inline=True)
@@ -39,14 +40,11 @@ async def server_status(address):
             if 'preset' in data: 
                 embed.add_field(name="Предустановленный режим", value=data['preset'], inline=True)
 
-        if data['tags']: 
-            embed.add_field(name="Теги сервера", value=", ".join(data['tags']), inline=False)
-        else:
-            embed.add_field(name="Теги сервера", value="Не указаны", inline=False)
-
-        if 'panic_bunker' in data: 
+        if 'tags' in data:
+            embed.add_field(name="Теги сервера", value=", ".join(data['tags']) if data['tags'] else "Не указаны", inline=False)
+        if 'panic_bunker' in data:
             embed.add_field(name="Режим бункера", value="Действует" if data['panic_bunker'] else "Отключён", inline=True)
-        if 'baby_jail' in data: 
+        if 'baby_jail' in data:
             embed.add_field(name="Режим тюрьмы", value="Действует" if data['baby_jail'] else "Отключён", inline=True) 
         return embed
 
@@ -57,3 +55,10 @@ async def server_status(address):
         return embed
 
 
+async def skibidi_station():
+    embed = disnake.Embed(title="ЩКЕБЕДЕ ДОП ДОП ЕС ЕС", color=disnake.Color.yellow())
+    embed.set_image(url="https://media.tenor.com/WhXYjpREz1kAAAAM/skibidi-toilet-skibidi.gif")
+    embed.set_thumbnail(url="https://steamuserimages-a.akamaihd.net/ugc/2038494310206033069/2FFBA39BB754CF0D0FC83C62636BAE30BE992758/")
+    embed.description = "Бррр, щкебеде, доп доп доп, ес ес \nСкибиди дабиди да дап \nSkibidi, dom-dom-dom, yes-yes \nSkibidi-dabudu, nib-nib \nWe ain't here to hurt nobody (skibidi, skibidi, skibidi) \nWanna see you work your body (skibidi, skibidi, skibidi)"
+    return embed
+    # этот мир больше не спасти
